10971d54963f3b53662e1c24e7ea20eb3b4fa1f4
janolk <jan.olkowski.a@gmail.com>
2024-07-30 18:05:39 -0700

    150: 4x4 cord

--- execution/executor-service/src/remote_cordinator_client.rs
+++ execution/executor-service/src/remote_cordinator_client.rs
@@ -55,7 +55,7 @@ impl RemoteCoordinatorClient {
         let execute_result_type = format!("execute_result_{}", shard_id);
         let command_rx = controller.create_inbound_channel(execute_command_type);
 
-        let num_result_tx_threads = 20;
+        let num_result_tx_threads = 30;
         let mut result_tx = vec![];
         for _ in 0..num_result_tx_threads {
             result_tx.push(Arc::new(tokio::sync::Mutex::new(OutboundRpcHelper::new(controller.get_self_addr(), coordinator_address, controller.get_outbound_rpc_runtime()))));
@@ -63,7 +63,7 @@ impl RemoteCoordinatorClient {
         let cmd_rx_thread_pool = Arc::new(
             rayon::ThreadPoolBuilder::new()
                 .thread_name(move |index| format!("remote-state-view-shard-send-request-{}-{}", shard_id, index))
-                .num_threads(num_cpus::get() / 2)
+                .num_threads(4)
                 .build()
                 .unwrap(),
         );
@@ -71,7 +71,7 @@ impl RemoteCoordinatorClient {
         let result_tx_thread_pool = Arc::new(
             rayon::ThreadPoolBuilder::new()
                 .thread_name(move |index| format!("remote-state-view-shard-send-request-{}-{}", shard_id, index))
-                .num_threads(num_cpus::get() / 2)
+                .num_threads(4)
                 .build()
                 .unwrap(),
         );
